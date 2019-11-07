f0b4f7711b43d6abba616d9deee35aef091c4988
Thinkman Wang <wangxf1985@gmail.com>
2019-11-07 16:37:51 +0800

    update

--- app/src/main/java/com/shiyculture/coupon/MainActivity.java
+++ app/src/main/java/com/shiyculture/coupon/MainActivity.java
@@ -3,9 +3,12 @@ package com.shiyculture.coupon;
 import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.tbruyelle.rxpermissions2.RxPermissions;
+import com.tencent.smtt.sdk.WebChromeClient;
 import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
 import com.vondear.rxtool.RxDeviceTool;
 
 import butterknife.BindView;
@@ -34,13 +37,49 @@ public class MainActivity extends AppCompatActivity {
                 , Manifest.permission.ACCESS_WIFI_STATE)
                 .subscribe(granted -> {
                     if (granted) {
-                        String szIMEI = RxDeviceTool.getIMEI(MainActivity.this);
-                        String szUrl = String.format("https://coupon.shiyculture.com/?user_id=%s&app_key=609d544b98d347a5bcc17637c529b95a&app_secret=609d544b98d347a5bcc17637c529b95a&allow_buy=1", szIMEI);
-
-                        m_wvMain.loadUrl(szUrl);
+                        initWebView();
                     } else {
 
                     }
                 });
+    }
+
+    public void initWebView() {
+        m_wvMain.getSettings().setJavaScriptEnabled(true);
+        m_wvMain.getSettings().setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+        m_wvMain.getSettings().setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        m_wvMain.getSettings().setDisplayZoomControls(true); //隐藏原生的缩放控件
+        m_wvMain.getSettings().setBlockNetworkImage(false);//解决图片不显示
+        m_wvMain.getSettings().setLoadsImagesAutomatically(true); //支持自动加载图片
+        m_wvMain.getSettings().setDefaultTextEncodingName("utf-8");//设置编码格式
+
+        String szIMEI = RxDeviceTool.getIMEI(MainActivity.this);
+        String szUrl = String.format("https://coupon.shiyculture.com/?user_id=%s&app_key=609d544b98d347a5bcc17637c529b95a&app_secret=609d544b98d347a5bcc17637c529b95a&allow_buy=1", szIMEI);
+
+        m_wvMain.loadUrl("http://soft.imtt.qq.com/browser/tes/feedback.html");
+
+        m_wvMain.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String s) {
+                webView.loadUrl(s);
+                return true;
+            }
+        });
+
+        m_wvMain.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView webView, int i) {
+//                if (i < 100 && MainTaskFragment.this.isVisible()) {
+//                    tvTaskProgress.setVisibility(View.VISIBLE);
+//                    webView.setVisibility(View.GONE);
+//                } else {
+//                    if (MainTaskFragment.this.isVisible()) {
+//                        tvTaskProgress.setVisibility(View.GONE);
+//                        webView.setVisibility(View.VISIBLE);
+//                    }
+//                }
+            }
+        });
     }
 }
--- app/src/main/java/com/shiyculture/coupon/MyApp.java
+++ app/src/main/java/com/shiyculture/coupon/MyApp.java
@@ -2,6 +2,7 @@ package com.shiyculture.coupon;
 
 import android.app.Application;
 
+import com.tencent.smtt.sdk.QbSdk;
 import com.vondear.rxtool.RxTool;
 
 public class MyApp extends Application {
@@ -9,6 +10,20 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
 
-        RxTool.init(this);
+        RxTool.init(getApplicationContext());
+
+        QbSdk.setDownloadWithoutWifi(true);
+        //x5内核初始化接口
+        QbSdk.initX5Environment(getApplicationContext(), new QbSdk.PreInitCallback() {
+            @Override
+            public void onCoreInitFinished() {
+
+            }
+
+            @Override
+            public void onViewInitFinished(boolean b) {
+
+            }
+        });
     }
 }
